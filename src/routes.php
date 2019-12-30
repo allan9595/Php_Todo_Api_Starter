62f40737ebf3c918c7e52f60ea793ff213fd49ba
*allan9595* <bohanzhang@my.unt.edu>
2019-12-30 16:26:52 -0500

    add deleting subtasks when primary task deleted, function renaming

--- src/routes.php
+++ src/routes.php
@@ -66,7 +66,7 @@ $app->group('/api/v1/todos', function() use($app) {
 
         //add a subtasks
         $app->post('', function ($request, $response, $args) {
-            $this->task->getTask($args['id']);
+            $this->task->getTask($args['task_id']); //check if the primart task exist
             $data = $request->getParsedBody();
             $data['taskId'] = $args['task_id']; //assign the id to the data['id']
             $result = $this->subtask->addSubtaskByTaskId($data);
